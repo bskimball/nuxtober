1542e19e195e860249d1d0115c012f38823dbb7c
Brian Kimball <brian.kimball@bdkonline.com>
2018-09-12 17:11:31 -0400

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,16 +6,7 @@ module.exports = {
   head: {
     htmlAttrs: {
       lang: 'en-US'
-    },
-    title: 'BDK',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'BDK' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    }
   },
   generate: {
     dir: 'pages'
