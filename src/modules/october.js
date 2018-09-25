44d01fa62dbe2b319a217cd29a23fbd8c5c57fd3
Brian Kimball <brian.kimball@bdkonline.com>
2018-09-25 09:12:03 -0400

    Update october.js

--- src/modules/october.js
+++ src/modules/october.js
@@ -1,11 +1,15 @@
 const fsExtra = require("fs-extra");
+const path = require("path");
 
 module.exports = function() {
   this.nuxt.hook("generate:before", async nuxt => {
-    nuxt.distNuxtPath = process.cwd() + "\\assets\\";
+    nuxt.distPath = (await process.cwd()) + "\\pages";
+    nuxt.distNuxtPath = (await process.cwd()) + "\\assets\\";
   });
   this.nuxt.hook("generate:distRemoved", async nuxt => {
     await fsExtra.remove(nuxt.distNuxtPath);
+    const nojekyllPath = await path.resolve(nuxt.distPath, ".nojekyll");
+    await fsExtra.createFile(nojekyllPath);
   });
   this.nuxt.hook("generate:page", async ctx => {
     let pageTitle = ctx.html.match(/<title data-n-head="true">(.*?)<\/title>/);
